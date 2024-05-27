a0b5108288fa4e62a565e090af6d2351d608b76e
Dzmitry Skryliou <dzmitry_skryliou@epam.com>
2024-05-27 16:02:47 +0200

    APItests

---
+++ Core/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using Core;
+using log4net;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Core
+
+{
+    public class BaseTest
+    {
+        protected static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [OneTimeSetUp]
+        public void BaseSetUp()
+        {
+            LogHelper.InitializeLogger();
+        }
+    }
+}
---
+++ Core/logHelper.cs
@@ -0,0 +1,18 @@
+﻿using log4net;
+using log4net.Config;
+
+namespace Core
+{
+    public class LogHelper
+    {
+        public static void InitializeLogger()
+        {
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+        }
+
+        public static ILog GetLogger(Type type)
+        {
+            return LogManager.GetLogger(type);
+        }
+    }
+}
